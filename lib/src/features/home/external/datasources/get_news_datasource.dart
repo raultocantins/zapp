6ffeb7bea7eae61a5702508db1547408fe299c8e
Alex Raul <alexbraul.ar@gmail.com>
2023-04-15 17:20:43 -0300

    change host api

--- lib/src/features/home/external/datasources/get_news_datasource.dart
+++ lib/src/features/home/external/datasources/get_news_datasource.dart
@@ -11,7 +11,7 @@ class GetNewsDatasourceImpl implements GetNewsDatasource {
       dio.options.headers["filter"] = tag;
       dio.options.headers["page"] = page.toString();
       dio.options.headers["pageSize"] = pageSize.toString();
-      final response = await dio.get('http://192.168.100.5:3000/news');
+      final response = await dio.get('https://zback.herokuapp.com/news');
 
       return NewsModel.fromJson(response.data);
     } catch (e) {
